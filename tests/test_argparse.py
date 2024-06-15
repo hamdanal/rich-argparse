hamdanal/rich-argparse

commit 47869f67f50e1313da3bb716dbdd116545a60487

Fix `%(default)s` style when help markup is deactivated (#123)
Fixes
https://github.com/hamdanal/rich-argparse/issues/121#issuecomment-2151415554

rich_argparse/__init__.py
@@ -370,13 +370,14 @@ class RichHelpFormatter(argparse.HelpFormatter):
         # raise ValueError if needed
         help_string % params  # pyright: ignore[reportUnusedExpression]
         parts = []
+        default_span: r.Span | None = None
         last = 0
         for m in self._printf_style_pattern.finditer(help_string):
             start, end = m.span()
             parts.append(help_string[last:start])
             sub = r.escape(help_string[start:end] % params)
             if m.group("mapping") == "default":
-                sub = f"[argparse.default]{sub}[/argparse.default]"
+                default_span = r.Span(start, start + len(sub), "argparse.default")
             parts.append(sub)
             last = end
         parts.append(help_string[last:])
@@ -385,6 +386,8 @@ class RichHelpFormatter(argparse.HelpFormatter):
             if self.help_markup
             else r.Text("".join(parts), style="argparse.help")
         )
+        if default_span:
+            rich_help.spans.append(default_span)
         for highlight in self.highlights:
             rich_help.highlight_regex(highlight, style_prefix="argparse.")
         return rich_help

tests/test_argparse.py
@@ -1006,7 +1006,7 @@ def test_disable_help_markup():
     parser = ArgumentParser(
         prog="PROG", formatter_class=RichHelpFormatter, description="[red]Description text.[/]"
     )
-    parser.add_argument("--foo", help="[red]Help text.[/]")
+    parser.add_argument("--foo", default="def", help="[red]Help text (default: %(default)s).[/]")
     with patch.object(RichHelpFormatter, "help_markup", False):
         help_text = parser.format_help()
     expected_help_text = """\
@@ -1016,7 +1016,7 @@ def test_disable_help_markup():
 
     Optional Arguments:
       -h, --help  show this help message and exit
-      --foo FOO   [red]Help text.[/]
+      --foo FOO   [red]Help text (default: def).[/]
     """
     assert help_text == clean(expected_help_text)
 

commit fdcc1794a7eb620cd426178c5319acc42d393ea9

Fix `(default)s` style, again (#124)
Fix a regression in `%(default)s` style introduced in #123

rich_argparse/__init__.py
@@ -370,14 +370,18 @@ class RichHelpFormatter(argparse.HelpFormatter):
         # raise ValueError if needed
         help_string % params  # pyright: ignore[reportUnusedExpression]
         parts = []
-        default_span: r.Span | None = None
+        defaults: list[str] = []
+        default_repl = "rich-argparse-f3ae8b55df34d5d83a8189d2e4766e68-argparse-rich"
         last = 0
         for m in self._printf_style_pattern.finditer(help_string):
             start, end = m.span()
             parts.append(help_string[last:start])
-            sub = r.escape(help_string[start:end] % params)
+            sub = help_string[start:end] % params
             if m.group("mapping") == "default":
-                default_span = r.Span(start, start + len(sub), "argparse.default")
+                defaults.append(sub)
+                sub = default_repl
+            else:
+                sub = r.escape(sub)
             parts.append(sub)
             last = end
         parts.append(help_string[last:])
@@ -386,8 +390,14 @@ class RichHelpFormatter(argparse.HelpFormatter):
             if self.help_markup
             else r.Text("".join(parts), style="argparse.help")
         )
-        if default_span:
-            rich_help.spans.append(default_span)
+        default_repl_len = len(default_repl)
+        for default in reversed(defaults):
+            default_index = rich_help.plain.rindex(default_repl)
+            rich_help = (
+                rich_help[:default_index]
+                .append(default, style="argparse.default")
+                .append(rich_help[default_index + default_repl_len :])
+            )
         for highlight in self.highlights:
             rich_help.highlight_regex(highlight, style_prefix="argparse.")
         return rich_help

tests/test_argparse.py
@@ -1038,3 +1038,22 @@ def test_disable_text_markup():
       --foo FOO   Help text.
     """
     assert help_text == clean(expected_help_text)
+
+
+@pytest.mark.usefixtures("force_color")
+def test_arg_default_spans():
+    parser = ArgumentParser(prog="PROG", formatter_class=RichHelpFormatter)
+    parser.add_argument(
+        "--foo",
+        default="def",
+        help="(default: %(default)r) [red](default: %(default)s)[/] (default: %(default)s)",
+    )
+    expected_help_text = """\
+    \x1b[38;5;208mUsage:\x1b[0m \x1b[38;5;244mPROG\x1b[0m [\x1b[36m-h\x1b[0m] [\x1b[36m--foo\x1b[0m \x1b[38;5;36mFOO\x1b[0m]
+
+    \x1b[38;5;208mOptional Arguments:\x1b[0m
+      \x1b[36m-h\x1b[0m, \x1b[36m--help\x1b[0m  \x1b[39mshow this help message and exit\x1b[0m
+      \x1b[36m--foo\x1b[0m \x1b[38;5;36mFOO\x1b[0m   \x1b[39m(default: \x1b[0m\x1b[3;39m'def'\x1b[0m\x1b[39m) \x1b[0m\x1b[31m(default: \x1b[0m\x1b[3;39mdef\x1b[0m\x1b[31m)\x1b[0m\x1b[39m (default: \x1b[0m\x1b[3;39mdef\x1b[0m\x1b[39m)\x1b[0m
+    """
+    help_text = parser.format_help()
+    assert help_text == clean(expected_help_text)